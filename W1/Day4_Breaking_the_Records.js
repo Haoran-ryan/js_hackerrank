477175deaf13c7b8d264df37853eccf587cfce9b
Haoran-ryan <90777296+Haoran-ryan@users.noreply.github.com>
2023-07-16 23:26:40 +1000

    w1d4 question added

--- W1/Day4_Breaking_the_Records.js
+++ W1/Day4_Breaking_the_Records.js
@@ -1,3 +1,19 @@
+/*
+Maria plays college basketball and wants to go pro. Each season she maintains a record of her play. She tabulates the number of times she breaks her season record for most points and least points in a game. Points scored in the first game establish her record for the season, and she begins counting from there. Points scored in the future games are compared to her record.
+
+Example:
+scores = [12, 24, 10, 24]
+Scores are in the same order as the games played. She tabulates her results as follows:
+    Game Score  Minimum  Maximum   Min Max
+    0      12     12       12       0   0
+    1      24     12       24       0   1
+    2      10     10       24       1   1
+    3      24     10       24       1   1
+
+ Given the scores for a season, find and print the number of times Maria breaks her records for most and least points scored during the season.
+ Complete the breakingRecords function in the editor below. It must return an integer array containing the numbers of times she broke her records. Index 0 is for breaking most points records, and index 1 is for breaking least points records.
+ */
+
 function breakingRecords(scores) {
     // Write your code here
     let mininum = scores[0];
