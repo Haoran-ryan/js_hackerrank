7f3fb54a3de8af4f276ca84f9c82f89c7026a94a
Haoran-ryan <90777296+Haoran-ryan@users.noreply.github.com>
2023-07-26 12:10:11 +1000

    w2d2

---
+++ W2/d2_grading_students.js
@@ -0,0 +1,60 @@
+/*
+HackerLand University has the following grading policy:
+- Every student receives a grade in the inclusive range from 0 to 100.
+- Any grade less than 40 is a failing grade.
+
+Sam is a professor at the university and likes to round each student's grade according to these rules:
+- If the difference between the grade and the next multiple of 5 is less than 3, round grade up to the next multiple of 5.
+- If the value of grade is less than 38, no rounding occurs as the result will still be a failing grade.
+
+Examples:
+- grade = 84 round to 85 (85 - 84 is less than 3)
+- grade = 29 do not round (result is less than 40)
+- grade = 57 do not round (60 - 57 is 3 or higher)
+
+Given the initial value of grade for each of Sam's n students, write code to automate the rounding process.
+
+Function Description:
+Complete the function gradingStudents in the editor below.
+gradingStudents has the following parameter(s):
+- int grades[n]: the grades before rounding
+
+Returns:
+- int[n]: the grades after rounding as appropriate
+
+Input Format:
+The first line contains a single integer, n, the number of students.
+Each line i of the n subsequent lines contains a single integer, grades[i].
+
+Constraints:
+- 1 <= n <= 60
+- 0 <= grades[i] <= 100
+*/
+
+function gradingStudents(grades) {
+    if(grades < 38){
+        return grades
+    }
+   // modulo operation: last digit 
+   const last = grades % 10 
+   const secondLast = (grades - last) % 100 
+
+   if(last === 0){
+    return grades
+   }else if(last < 5 && last >=3){
+    return secondLast + 5
+   }else if(last > 7){
+    
+    if(secondLast === 90){
+        return 100
+    }else{
+        return secondLast + 10
+
+    }
+   }else{
+    return grades
+   }
+};
+
+
+console.log(gradingStudents(73))
